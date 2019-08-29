9c544a40f2bdc30527358ca3b1eae0e65f3478ec
KheniApexa <45095454+KheniApexa@users.noreply.github.com>
2019-08-29 19:21:11 +0530

    initial version

---
+++ dw-button.js
@@ -0,0 +1,96 @@
+/**
+@license
+Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+import { css } from 'lit-element';
+
+// These are the mwc element needed by this element.
+import { Button } from "@material/mwc-button"
+
+export class DWButton extends Button {
+  static get styles() {
+    return [
+      Button.styles,
+      css`
+        /** normal button css start */
+        :host(.secondary) .mdc-button  {
+          --mdc-theme-primary: var(--mdc-theme-secondary);
+        }
+        :host(.error) .mdc-button  {
+          --mdc-theme-primary: var(--mdc-theme-error);
+        }
+        :host([raised].secondary) .mdc-button--raised:not(:disabled) {
+          --mdc-theme-primary:  var(--mdc-theme-surface);
+          --mdc-theme-on-primary:  var(--mdc-theme-secondary);
+        }
+        :host([raised].error) .mdc-button--raised:not(:disabled) {
+          --mdc-theme-primary:  var(--mdc-theme-surface);
+          --mdc-theme-on-primary:  var(--mdc-theme-error);
+        }
+        :host([disabled]) .mdc-button:disabled  {
+          color: var(--mdc-theme-text-disabled);
+        }
+
+        /** primary-text button  css start */
+        :host(.text) .mdc-button  {
+          --mdc-theme-primary: var(--mdc-theme-text-primary);
+        }
+
+        /** outline button  css start */
+        :host([outlined]) .mdc-button--outlined:disabled  {
+          --mdc-theme-primary: var(--mdc-theme-secondary);
+        }
+        :host([error]) .mdc-button--outlined:disabled  {
+          --mdc-theme-primary: var(--mdc-theme-error);
+        }
+        :host([disabled][outlined]) .mdc-button--outlined:disabled  {
+          border-color: var(--mdc-theme-text-disabled);
+        }
+
+        /** filled button start */
+        :host([filled]) .mdc-button:not(:disabled){
+          background-color: var(--mdc-theme-primary);
+          color: var(--mdc-theme-on-primary); 
+        }
+        :host([filled].secondary) .mdc-button:not(:disabled) {
+          background-color: var(--mdc-theme-secondary);
+          color: var(--mdc-theme-on-secondary); 
+        }
+        :host([filled][disabled]) .mdc-button:disabled {
+          background-color: var( --dw-disabled-background-color, var(--mdc-theme-divider-on-light));
+        }
+        `
+    ];
+  }
+
+  static get properties() {
+    return {
+      
+      /**
+       * When you want to filled this button set to `true`.
+       */
+      filled : {
+        type: Boolean,
+        reflect: true
+      }
+    }
+  }
+
+  constructor(){
+    super();
+    this.filled = false;
+
+    /**
+    * call blur method to fix ripple effect after button click.
+    */
+    this.addEventListener('click', () => { this.blur(); })
+  }
+}
+
+window.customElements.define('dw-button', DWButton);
--- readme.md
+++ readme.md
@@ -1,1 +1,42 @@
-##dw-button
+# dw-button
+
+A button web component created using [`mwc-button`](https://www.webcomponents.org/element/@material/mwc-button)
+
+
+## [Demo](https://dreamworldsolutions.github.io/dw-button/demo/index.html)
+
+## Install
+```html
+npm install @dreamworld/dw-button
+```
+
+## Usage
+```html
+import '@dreamworld/dw-button/dw-button.js';
+
+<dw-button label="OK"></dw-button>
+```
+
+### Colors
+Button is rendered by default in Primary color. It can be rendered in secondary, error or text color too. For that 
+add corresponding class (`secondary`, `error` and `text`) to the button. e.g.
+
+```html
+<dw-button label="OK" class="secondary"></dw-button>
+```
+
+> it's actually main reason for creating extension of `mwc-button`.
+
+### Styles
+- To show button in outlined style add `outlined` attribute.
+- To show button in filled style, add `filled` attribute.
+- Additionally `raised` attribute can be used to show elevated button. As `outlined` button will never be raised, this
+doesn't work with `outlined` style. 
+
+
+### Disable
+Add `disabled` attribute to make a button disabled.
+
+## Known Issues
+- It's not possible to show **Normal + Raised** button in **Primary** color. It's due to the Custom CSS Property names
+used by `mwc-button` element.
